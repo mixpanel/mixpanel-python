0e977e68d11bc8b63700583f19376fac32a9c757
Amy Quispe <amy@mixpanel.com>
2013-06-21 01:49:02 +0000

    fixed a few things with initial unit testing.

--- mixpanel.py
+++ mixpanel.py
@@ -1,6 +1,11 @@
+import urllib
+import urllib2
+import base64
+import json
+
 class Mixpanel:
-    TRACK_ENDPOINT = "http://api.mixpanel.com/track/"
-    ENGAGE_ENDPOINT = "http://api.mixpanel.com/engage/"
+    _track_endpoint = "http://api.mixpanel.com/track/"
+    _engage_endpoint = "http://api.mixpanel.com/engage/"
     _token = None
     _distinct_id = None
 
@@ -8,7 +13,7 @@ class Mixpanel:
         _token = token
 
     def _write_request(self, endpoint, request):
-        data = urllib.urlencode({'data': base64.b64encode(json.dumps(record))})
+        data = urllib.urlencode({'data': base64.b64encode(json.dumps(request))})
         try:
             response = urllib2.urlopen(endpoint, data).read()
         except urllib2.HTTPError as e:
@@ -22,13 +27,13 @@ class Mixpanel:
             raise RuntimeError('%s failed', endpoint)
 
     def _write_event(self, event):
-        self._write_request(TRACK_ENDPOINT, event)
+        self._write_request(self._track_endpoint, event)
 
     def _write_record(self, record, distinct_id):
         self._distinct_id = distinct_id
-        self._write_request(ENGAGE_ENDPOINT, record)
+        self._write_request(self._engage_endpoint, record)
 
-    def track(event_name, properties, ip=0, verbose=False):
+    def track(self, event_name, properties, ip=0, verbose=False):
         properties.update( {"ip": ip, "verbose": verbose} )
         event = {
             "event": event_name,
@@ -39,41 +44,41 @@ class Mixpanel:
     def identify(self, distinct_id):
         self._distinct_id = distinct_id
 
-    def _engage_update(self, update_type, properties, distinct_id=self._distinct_id):
+    def _engage_update(self, update_type, properties, distinct_id=_distinct_id):
         record = {
             "token": self._token,
-            "$distinct_id": self._distinct_id,
+            "$distinct_id": distinct_id,
              update_type: properties,
         }
         self._write_record(record)
 
-    def alias(self, alias_id, distinct_id=self._distinct_id):
+    def alias(self, alias_id, distinct_id=_distinct_id):
         record = {
             "event": "$create_alias",
             "properties": {
-                "distinct_id": self._distinct_id,
+                "distinct_id": distinct_id,
                 "alias": alias_id,
                 "token": self._token,
             } 
         }
 
-    def people_set(self, properties, distinct_id=self._distinct_id):
+    def people_set(self, properties, distinct_id=_distinct_id):
         self._engage_update("$set", properties, distinct_id)
 
-    def people_set_once(self, properties, distinct_id=self._distinct_id):
+    def people_set_once(self, properties, distinct_id=_distinct_id):
         self._engage_update("$set_once", properties, distinct_id)
 
-    def people_add(self, properties, distinct_id=self._distinct_id):
+    def people_add(self, properties, distinct_id=_distinct_id):
         self._engage_update("$add", properties, distinct_id)
 
-    def people_append(self, properties, distinct_id=self._distinct_id):
+    def people_append(self, properties, distinct_id=_distinct_id):
         self._engage_update("$append", properties, distinct_id)
 
-    def people_union(self, properties, distinct_id=self._distinct_id):
+    def people_union(self, properties, distinct_id=_distinct_id):
         self._engage_update("$union", properties, distinct_id)
 
-    def people_unset(self, properties, distinct_id=self._distinct_id):
+    def people_unset(self, properties, distinct_id=_distinct_id):
         self._engage_update("$unset", properties, distinct_id)
 
-    def people_delete(self, distinct_id=self._distinct_id):
+    def people_delete(self, distinct_id=_distinct_id):
         self._engage_update("$delete", "", distinct_id)
