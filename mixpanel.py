3132fdf2b6146a47ace0f900792066f63d3f9bc6
Amy Quispe <amy@mixpanel.com>
2013-06-21 00:07:22 +0000

    fixed first round of changes (mostly style) thanks to joe and josh

--- mixpanel.py
+++ mixpanel.py
@@ -1,78 +1,73 @@
-TRACK_ENDPOINT = "http://api.mixpanel.com/track/"
-ENGAGE_ENDPOINT = "http://api.mixpanel.com/engage/"
-
 class Mixpanel:
-    __token = None
-    __distinct_id = None
+    TRACK_ENDPOINT = "http://api.mixpanel.com/track/"
+    ENGAGE_ENDPOINT = "http://api.mixpanel.com/engage/"
+    _token = None
+    _distinct_id = None
 
-
-    def __write_request__(self, endpoint, request):
+    def _write_request(self, endpoint, request):
         data = urllib.urlencode({'data': base64.b64encode(json.dumps(record))})
-        ENDPOINT = ENGAGE_ENDPOINT if (endpoint == "engage") else TRACK_ENDPOINT
         try:
-            response = urllib2.urlopen(ENDPOINT, data).read()
+            response = urllib2.urlopen(endpoint, data).read()
         except urllib2.HTTPError as e:
+            # remove when done with development
             print e.read()
             raise
         if response == '1':
-            # will have to change this
+            # remove when done with development 
             print 'success' 
         else:
             raise RuntimeError('%s failed', endpoint)
 
-    def __write_event__(self, event):
-        self.__write_request__('track', event)
+    def _write_event(self, event):
+        self._write_request(TRACK_ENDPOINT, event)
 
-    def __write_record__(self, record):
-        self.__write_request__('engage', record)
+    def _write_record(self, record):
+        self._write_request(ENGAGE_ENDPOINT, record)
 
     def track(event_name, properties, ip=0, verbose=False):
         properties.update( {"ip": ip, "verbose": verbose} )
         event = {
-                    "event": event_name,
-                    "properties": properties, 
-                }
-        self.__write_event__(event)
+            "event": event_name,
+            "properties": properties, 
+        }
+        self._write_event(event)
 
-    def __engage_update_(self, update_type, properties):
+    def _engage_update(self, update_type, properties):
         record = {
-                     "token": self.__token,
-                     "$distinct_id": self.__distinct_id,
-                     update_type: properties,
-                 }
-        self.__write_record__(record)
+            "token": self._token,
+            "$distinct_id": self._distinct_id,
+             update_type: properties,
+        }
+        self._write_record(record)
 
-    def identify(self, some_id):
-        self.__distinct_id = some_id
-
-    def alias(self, alias_id, original=__distinct_id):
+    def alias(self, alias_id, original=_distinct_id):
         record = {
-                     "event": "$create_alias",
-                     "properties": {
-                         "distinct_id": self.__distinct_id,
-                         "alias": alias_id,
-                         "token": self.__token,
-                     } 
-                 }
+            "event": "$create_alias",
+            "properties": {
+                "distinct_id": self._distinct_id,
+                "alias": alias_id,
+                "token": self._token,
+            } 
+        }
 
     def people_set(self, properties):
-        __engage_update__(self, "$set", properties)
+        self._engage_update(self, "$set", properties)
 
     def people_set_once(self, properties):
-        __engage_update__(self, "$set_once", properties)
+        self._engage_update(self, "$set_once", properties)
 
     def people_add(self, properties):
-        __engage_update__(self, "$add", properties)
+        self._engage_update(self, "$add", properties)
 
     def people_append(self, properties):
-        __engage_update__(self, "$append", properties)
+        self._engage_update(self, "$append", properties)
 
     def people_union(self, properties):
-        __engage_update__(self, "$union", properties)
+        self._engage_update(self, "$union", properties)
 
     def people_unset(self, properties):
-        __engage_update__(self, "$unset", properties)
+        self._engage_update(self, "$unset", properties)
 
     def people_delete(self):
-        __engage_update__(self, "$delete", "")
+        self._engage_update(self, "$delete", "")
 
