d04af84483c206f2f4298aef2789b73300e1c7cd
Amy Quispe <amy@mixpanel.com>
2013-07-04 02:13:53 +0000

    much more commented

---
+++ mixpanel.py
@@ -0,0 +1,221 @@
+import base64
+import json
+import urllib
+import urllib2
+
+class Mixpanel(object):
+
+    def __init__(self, token, base_url='https://api.mixpanel.com/'):
+        """
+        Creates a new Mixpanel object, which can be used for all tracking.
+
+        To use mixpanel, create a new Mixpanel object using your token.
+        Use this object to start tracking.
+        Takes in a user token and an optional base url. Base url defaults to
+        https://api.mixpanel.com.
+        Example:
+            mp = Mixpanel('36ada5b10da39a1347559321baf13063')
+        """
+        self._token = token
+        self._base_url = base_url
+
+    @classmethod
+    def _prepare_data(self, data):
+        return urllib.urlencode({'data': base64.b64encode(json.dumps(data)),'verbose':1})
+
+    def _write_request(self, base_url, endpoint, request, batch=False):
+        data = self._prepare_data(request)
+        request_url = ''.join([base_url, endpoint])
+        try:
+            if not batch:
+                response = urllib2.urlopen(request_url, data).read()
+            else:
+                batch_request = urllib2.Request(request_url, data)
+                response = urllib2.urlopen(batch_request).read()
+
+        except urllib2.HTTPError as e:
+            raise e
+        return response == '1'
+
+    def _people(self, distinct_id, update_type, properties):
+        record = {
+            '$token': self._token,
+            '$distinct_id': distinct_id,
+            update_type: properties,
+        }
+        return self._write_request(self._base_url, 'engage/', record)
+
+    def track(self, event_name, properties={}):
+        """
+        For all event tracking.
+
+        For basic event tracking. Should pass in name of event name and
+        optionally a dictionary of properties.
+        Example:
+            mp.track('clicked button', { 'color': 'blue', 'text': 'no' })
+        """
+        all_properties = { 'token' : self._token }
+        all_properties.update(properties)
+        event = {
+            'event': event_name,
+            'properties': all_properties,
+        }
+        return self._write_request(self._base_url, 'track/', event)
+
+    def alias(self, alias_id, original):
+        """
+        Gives custom alias to a people record. 
+
+        Allows use of alias_id instead of original alias. Both arguments are
+        required since people records are not persistent across multiple calls. 
+        Example:
+            mp.alias('amy@mixpanel.com', '13793')
+        """
+        record = {
+            'event': '$create_alias',
+            'properties': {
+                'distinct_id': original,
+                'alias': alias_id,
+                'token': self._token,
+            }
+        }
+        return self._write_request(self._base_url, 'engage/', record)
+
+    def people_set(self, distinct_id, properties):
+        """
+        Set properties of a people record.
+
+        Sets properties of a people record given in JSON object. If the profile
+        does not exist, creates new profile with these properties.
+        Example:
+            mp.people_set('12345', {'Address': '1313 Mockingbird Lane', 
+                                    'Birthday': '1948-01-01'
+                        })
+        """
+        return self._people(distinct_id, '$set', properties)
+
+    def people_set_once(self, distinct_id, properties):
+        """
+        Set immutable properties of a people record.
+
+        Sets properties of a people record given in JSON object. If the profile
+        does not exist, creates new profile with these properties. Does not
+        overwrite existing property values.
+        Example:
+            mp.people_set_once('12345', {'First Login': "2013-04-01T13:20:00"})
+        """
+        return self._people(distinct_id, '$set_once', properties)
+
+    def people_add(self, distinct_id, properties):
+        """
+        Increments/decrements numerical properties of people record. 
+
+        Takes in JSON object with keys and numerical values. Adds numerical
+        values to current property of profile. If property doesn't exist adds
+        value to zero. Takes in negative values for subtraction.
+        Example:
+            mp.people_add('12345', {'Coins Gathered': 12})
+        """
+        return self._people(distinct_id, '$add', properties)
+
+    def people_append(self, distinct_id, properties):
+        """
+        Appends to the list associated with a property. 
+
+        Takes a JSON object containing keys and values, and appends each to a
+        list associated with the corresponding property name. $appending to a
+        property that doesn't exist will result in assigning a list with one
+        element to that property.
+        Example:
+            mp.people_append('12345', { "Power Ups": "Bubble Lead" }) 
+        """
+       return self._people(distinct_id, '$append', properties)
+
+    def people_union(self, distinct_id, properties):
+        """
+        Merges the values for a list associated with a property. 
+
+        Takes a JSON object containing keys and list values. The list values in
+        the request are merged with the existing list on the user profile,
+        ignoring duplicate list values.
+        Example:
+            mp.people_union('12345', { "Items purchased": ["socks", "shirts"] } )
+        """
+        return self._people(distinct_id, '$union', properties)
+
+    def people_unset(self, distinct_id, properties):
+        """
+        Removes properties from a profile.
+
+        Takes a JSON list of string property names, and permanently removes the
+        properties and their values from a profile.
+        Example:
+            mp.people_unset('12345', ["Days Overdue"])
+        """
+        return self._people(distinct_id, '$unset', properties)
+
+    def people_delete(self, distinct_id):
+        """
+        Permanently deletes a profile. 
+
+        Permanently delete the profile from Mixpanel, along with all of its
+        properties.
+        Example:
+            mp.people_delete('12345')
+        """
+        return self._people(distinct_id, '$delete', "")
+
+    def send_people_batch(self, data):
+       """
+       Sends list of up to 50 people records.
+
+       Takes in list of JSON objects, up to 50 people records.
+       Example:
+       data = 
+           [
+               {
+                   "$token": "36ada5b10da39a1347559321baf13063",
+                   "$distinct_id": "13793",
+                   "$set": {
+                       "$first_name": "Joe",
+                       "$last_name": "Doe",
+                       "$email": "joe.doe@example.com",
+                       "$created": "2013-04-01T13:20:00",
+                       "$phone": "4805551212"
+                       }
+               }
+           ]
+       mp.send_people_batch(data)
+       """
+       return self._write_request(self._base_url, 'engage/', data, batch=True)
+
+    def send_events_batch(self, data):
+        """
+        Sends list of up to 50 events.
+
+        Takes in list of JSON objects, up to 50 events records.
+        Example:
+        data = 
+            [
+                {
+                    "event": "Signed Up",
+                    "properties": {
+                    "distinct_id": "13793",
+                    "token": "e3bc4100330c35722740fb8c6f5abddc",
+                    "Referred By": "Friend",
+                    "time": 1371002000
+                    }
+                },
+                {
+                    "event": "Uploaded Photo",
+                    "properties": {
+                    "distinct_id": "13793",
+                    "token": "e3bc4100330c35722740fb8c6f5abddc",
+                    "Topic": "Vacation",
+                    "time": 1371002104
+                    }
+                }
+            ]
+        mp.send_events_batch(data)
+        """
+        return self._write_request(self._base_url, 'track/', data, batch=True)
