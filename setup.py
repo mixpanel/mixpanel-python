ebbeba68cc7cbec6b061dee7f3ab99ed64d7152b
Joe Bowers <joe@mixpanel.com>
2015-02-05 19:17:44 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='mixpanel-py',
-    version='3.2.1',
+    version='4.0.0',
     author='Mixpanel, Inc.',
     author_email='dev@mixpanel.com',
     packages=['mixpanel'],
