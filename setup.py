fc72f939c2e3ed634840a5acb6610a14df975c7a
Zak Johnson <zakj@nox.cx>
2015-03-17 11:30:02 -0700

    DRY up version string in setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 from codecs import open
 from os import path
+import re
 from setuptools import setup, find_packages
 
-here = path.abspath(path.dirname(__file__))
+def read(*paths):
+    filename = path.join(path.abspath(path.dirname(__file__)), *paths)
+    with open(filename, encoding='utf-8') as f:
+        return f.read()
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+def find_version(*paths):
+    contents = read(*paths)
+    match = re.search(r'^__version__ = [\'"]([^\'"]+)[\'"]', contents, re.M)
+    if not match:
+        raise RuntimeError('Unable to find version string.')
+    return match.group(1)
 
 setup(
     name='mixpanel-py',
-    version='4.0.2',
+    version=find_version('mixpanel', '__init__.py'),
     description='Official Mixpanel library for Python',
-    long_description=long_description,
+    long_description=read('README.md'),
     url='https://github.com/mixpanel/mixpanel-python',
     author='Mixpanel, Inc.',
     author_email='dev@mixpanel.com',
