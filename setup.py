e8bd59015b3f4d6a9e3e17c8eaebd08c23078463
Zak Johnson <zakj@nox.cx>
2015-03-16 19:00:23 -0700

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from codecs import open
+from os import path
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='mixpanel-py',
     version='4.0.2',
+    description='Official Mixpanel library for Python',
+    long_description=long_description,
+    url='https://github.com/mixpanel/mixpanel-python',
     author='Mixpanel, Inc.',
     author_email='dev@mixpanel.com',
-    packages=['mixpanel'],
-    url='https://github.com/mixpanel/mixpanel-python',
-    description='Official Mixpanel library for Python',
-    long_description=open('README.md').read(),
+    license='Apache',
+
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2 :: Only',
-    ]
+    ],
+
+    keywords='mixpanel analytics',
+    packages=find_packages(),
 )
