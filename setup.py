1364bfb6bba3421523d923754280c61603dc68db
David Grant <david.grant@mixpanel.com>
2021-06-21 16:14:16 -0700

    Add python_requires arg.

--- setup.py
+++ setup.py
@@ -24,19 +24,24 @@ setup(
     author='Mixpanel, Inc.',
     author_email='dev@mixpanel.com',
     license='Apache',
+    python_requires='>=2.7, !=3.4.*',
     install_requires=[
         'six>=1.9.0',
         'requests>=2.4.2',
         'urllib3',
     ],
-
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
-
     keywords='mixpanel analytics',
     packages=find_packages(),
 )
