a4f18b59fbac9bad644c0c8331f97a0f125d76eb
Jared McFarland <jared@mixpanel.com>
2024-03-08 14:15:44 -0800

    add trailing comma

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12'
+        'Programming Language :: Python :: 3.12',
     ],
     keywords='mixpanel analytics',
     packages=find_packages(),
