cd2616988e0f6646d70d875f1d70090eb09ed48b
Jared McFarland <jared@mixpanel.com>
2024-03-08 14:36:24 -0800

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ master_doc = 'index'
 project = u'mixpanel'
 copyright = u' 2021, Mixpanel, Inc.'
 author = u'Mixpanel <dev@mixpanel.com>'
-version = release = '4.9.0'
+version = release = '4.10.1'
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 
